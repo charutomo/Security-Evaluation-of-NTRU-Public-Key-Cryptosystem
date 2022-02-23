cbaa5caf8eb6b010f59c747e1382ecc9c2a495de
Charissa <charissa.utomo@gmail.com>
2022-02-23 15:56:35 +0800

    LLL testing larger lattices and used numpy linear algebra to find norm

--- New Proposed Attack/LLL python implementation.py
+++ New Proposed Attack/LLL python implementation.py
@@ -8,21 +8,39 @@ Created on Sat Oct  2 21:25:39 2021
 #using lll attack
 
 import numpy as np
+from numpy.linalg import norm 
 import math
 
-l1 = np.array([1,0,1,0], dtype = ('float64'))
-l2 = np.array([1,1,1,1], dtype = ('float64'))
-l3 = np.array([0,1,2,1],dtype = ('float64'))
-latticelist = [l1,l2,l3]
+l1 = np.array([1,0,0,0,0,0,0,0,0,0,0,50,5,32,36,31,53,28,46,25,49,11], dtype = ('float64'))
+l2 = np.array([0,1,0,0,0,0,0,0,0,0,0,11,50,5,32,36,31,53,28,46,25,49], dtype = ('float64'))
+l3 = np.array([0,0,1,0,0,0,0,0,0,0,0,49,11,50,5,32,36,31,53,28,46,25], dtype = ('float64'))
+l4 = np.array([0,0,0,1,0,0,0,0,0,0,0,25,49,11,50,5,32,36,31,53,28,46], dtype = ('float64'))
+l5 = np.array([0,0,0,0,1,0,0,0,0,0,0,46,25,49,11,50,5,32,36,31,53,28], dtype = ('float64'))
+l6 = np.array([0,0,0,0,0,1,0,0,0,0,0,28,46,25,49,11,50,5,32,36,31,53], dtype = ('float64'))
+l7 = np.array([0,0,0,0,0,0,1,0,0,0,0,53,28,46,25,49,11,50,5,32,36,31], dtype = ('float64'))
+l8 = np.array([0,0,0,0,0,0,0,1,0,0,0,31,53,28,46,25,49,11,50,5,32,36], dtype = ('float64'))
+l9 = np.array([0,0,0,0,0,0,0,0,1,0,0,36,31,53,28,46,25,49,11,50,5,32], dtype = ('float64'))
+l10 = np.array([0,0,0,0,0,0,0,0,0,1,0,32,36,31,53,28,46,25,49,11,50,5], dtype = ('float64'))
+l11 = np.array([0,0,0,0,0,0,0,0,0,0,1,5,32,36,31,53,28,46,25,49,11,50], dtype = ('float64'))
+l12 = np.array([0,0,0,0,0,0,0,0,0,0,0,61,0,0,0,0,0,0,0,0,0,0], dtype = ('float64'))
+l13 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,61,0,0,0,0,0,0,0,0,0], dtype = ('float64'))
+l14 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,61,0,0,0,0,0,0,0,0], dtype = ('float64'))
+l15 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,61,0,0,0,0,0,0,0], dtype = ('float64'))
+l16 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,61,0,0,0,0,0,0], dtype = ('float64'))
+l17 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,61,0,0,0,0,0], dtype = ('float64'))
+l18 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,61,0,0,0,0], dtype = ('float64'))
+l19 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,61,0,0,0], dtype = ('float64'))
+l20 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,61,0,0], dtype = ('float64'))
+l21 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,61,0], dtype = ('float64'))
+l22 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,61], dtype = ('float64'))
+
+latticelist= [l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,l18,l19,l20,l21,l22]
 
 
-def norm(vector):
-    num = 0
-    for i in range(len(vector)):
-        num += (vector[i])**2
-    normvec = num**(1/2)
-    return normvec
 
+#finding norm
+# use norm(vector,2) for norm instead from numpy.linalg
+# for norm^2 use norm(vector,1)
 
 
 def gramschmidts(latticelist):
@@ -37,51 +55,59 @@ def gramschmidts(latticelist):
     Returns
     -------
     ortholist : list
-        outputs a list of orthonormal basis
+        outputs a list of orthogonal basis
 
     '''
     newlist = []
-    vector1 = latticelist[0]
-    newlist.append(vector1)
+    newlist.append(latticelist[0])
     for i in range(1,len(latticelist)):
         w= latticelist[i]
         nextvector =0
         nextvector += latticelist[i]
         for j in range(len(newlist)):
-            nextvector -= (np.dot(w,newlist[j])/((norm(newlist[j]))**2)) * newlist[j]
+            nextvector -= (np.dot(w,newlist[j])/(norm(newlist[j],1))) * newlist[j]
         newlist.append(nextvector)
-    ortholist = []
-    for k in range(len(newlist)):
-        ortholist.append(newlist[k]/norm(newlist[k]))
-    return ortholist
+    return newlist
 
 
 def lovasz(ortholist):
+    '''
+    
+
+    Parameters
+    ----------
+    ortholist : list 
+        gram schmidts orthogonal list
+
+    Returns
+    -------
+    ortholist : list
+        return ortholist that satisfy lovasz condition
+    boolean : bool
+        if it is true, there is a swap
+        otherwise, no swap
+
+    '''
     boolean = False
-    for i in range(len(ortholist)):
-        for j in range(len(ortholist)):
-            if i>j:
-                if np.dot(ortholist[i],ortholist[j])/((norm(ortholist[j]))**2) <= 0.5 :
-                    pass
-                else:
-                    temp = ortholist[i]
-                    ortholist[i] = ortholist[j]
-                    ortholist[j] = temp                
-    for k in range(1,len(ortholist)):
-        if (norm(ortholist[k-1]))**2> (norm(ortholist[k]))**2 + ((np.dot(ortholist[k],ortholist[k-1])/((norm(ortholist[k-1]))**2))**2)*norm((ortholist[k-1]))**2:
-            temp = ortholist[k-1]
-            ortholist[k-1] = ortholist[k]
-            ortholist[k] = temp 
+    for k in range(len(ortholist)-1):
+        if 0.75*norm(ortholist[k],1)> norm(ortholist[k+1]+(np.dot(ortholist[k+1],ortholist[k])/(norm(ortholist[k+1],1)))*ortholist[k],1):
+            temp = ortholist[k]
+            ortholist[k] = ortholist[k+1]
+            ortholist[k+1] = temp 
             boolean = True
+            break
     return ortholist, boolean
+
 
 def lllalgo(l):
    if lovasz(gramschmidts(l))[1] == True:
-       lllalgo(lovasz(gramschmidts(l))[0])
+       return lllalgo(lovasz(gramschmidts(l))[0])
    else:
-        return lovasz(gramschmidts(l))[0]
+       return lovasz(gramschmidts(l))[0]
         
 print(lllalgo(latticelist))   
+
+
      
 
     
